003af6dda87018ee8165d2c60ae16038c82d8063
Fahad Mohammed <fahadmoha2006@gmail.com>
2026-03-27 22:49:59 +0400

    Update database URI for configuration

--- database.py
+++ database.py
@@ -3,5 +3,5 @@ from flask_sqlalchemy import SQLAlchemy
 db = SQLAlchemy()
 
 class Config:
-    SQLALCHEMY_DATABASE_URI = "postgresql://postgres:X_aseelkhsroo_@localhost:5432/Dawa"
+    SQLALCHEMY_DATABASE_URI = "postgresql://postgres:password_@localhost:5432/database_name"
     SQLALCHEMY_TRACK_MODIFICATIONS = False
