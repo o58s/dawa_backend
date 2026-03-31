35d3cd7e9417a4f5201ace3a4a68d8e02799eee9
Fahad Mohammed <fahadmoha2006@gmail.com>
2026-03-31 20:24:16 +0400

    Register new blueprints for drug, disease, and protein
    Added new blueprints for drug, disease, and protein routes.

--- app.py
+++ app.py
@@ -2,7 +2,10 @@ from flask import Flask
 from database import db, Config
 from routes.auth import auth_bp
 from routes.search import search_bp
+from routes.drug import drug_bp
 from routes.prediction import prediction_bp
+from routes.dsisease import disease_bp
+from routes.protein import protein_bp
 
 app = Flask(__name__)
 app.config.from_object(Config)
@@ -12,7 +15,14 @@ db.init_app(app)
 
 app.register_blueprint(auth_bp)
 app.register_blueprint(search_bp)
+app.register_blueprint(drug_bp)
 app.register_blueprint(prediction_bp)
+app.register_blueprint(disease_bp)
+app.register_blueprint(protein_bp)
+
+@app.route("/")
+def home():
+    return "Backend is running"
 
 if __name__ == "__main__":
     app.run(debug=True)
